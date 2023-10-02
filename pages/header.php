5b53c33dc5b1206316dbd136c18440690f944c6d
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-02 12:58:03 -0600

    New pages

--- pages/header.php
+++ pages/header.php
@@ -1,14 +1,27 @@
-<header>
-    <img src="../images/logo.PNG" alt="logo" width="100" height="100">
-    <link rel="stylesheet" type="text/css" href="../css/header.css">
-    <link rel="icon" type="image/x-icon" href="../favicon/logo.icon">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
 
-</header>
-<nav>
-    <ul>
-        <li><a href="../pages/home.php">Home</a></li>
-        <li><a href="../pages/feed.php">Feed</a></li>
-        <li><a href="../pages/myprofile.php">My Profile</a></li>
-        <li><a href="../pages/about.php">About</a></li>
-    </ul>
-</nav>
+    <!-- Favicon -->
+    <link rel="icon" href="../favicon/logo.icon" type="image/x-icon">
+    <link rel="shortcut icon" href="../favicon/logo.icon" type="image/x-icon">
+
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+</head>
+<body>
+    <header>
+        <img src="../images/logo.PNG" alt="logo" width="100" height="100">
+    </header>
+    <nav>
+        <ul>
+            <li><a href="../pages/home.php">Home</a></li>
+            <li><a href="../pages/feed.php">Feed</a></li>
+            <li><a href="../pages/myprofile.php">My Profile</a></li>
+            <li><a href="../pages/about.php">About</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
