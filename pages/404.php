61e8f8b424fa73db74df9a33c784f956bfef90b9
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-02 13:08:29 -0600

    New pages

---
+++ pages/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../favicon/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../favicon/favicon.ico" type="image/x-icon">
+    <title>Error 404 - Page Not Found</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <div class="Error">
+        <h1>Error 404 - Page Not Found</h1>
+        <p>The page you are looking for could not be found.</p>
+        <p>Return to the <a href="../pages/home.php">homepage</a>.</p>
+    </div>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
