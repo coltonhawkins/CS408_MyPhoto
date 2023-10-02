2b44c57fb2418bfaac1cfa8365d4d22eadb0d9f0
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-01 20:00:18 -0600

    New pages

--- pages/header.php
+++ pages/header.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <title>Your Website Title</title>
+    <link rel="stylesheet" type="text/css" href="headerfooter.css">
+
+    <title>Home</title>
 </head>
 <body>
     <header>
