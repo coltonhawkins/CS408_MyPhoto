e71a364694d53f23b2a67350691a19a9cba5a097
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-01 19:54:15 -0600

    New pages

---
+++ pages/feed.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Photo Feed - My Foto</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+
+    <section id="feed-section">
+        <h2>Photo Feed</h2>
+
+        //Insert database code here
+        <?php include 'footer.php'; ?>
+</body>
+</html>
