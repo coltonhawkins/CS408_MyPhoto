7e60d350e194d7422dc8eecc76670dbc1d08d737
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-02 13:22:36 -0600

    New pages

--- pages/about.php
+++ pages/about.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="about.css">
     <link rel="icon" href="../favicon/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="../favicon/favicon.ico" type="image/x-icon">
     <title>About Us - My Foto</title>
