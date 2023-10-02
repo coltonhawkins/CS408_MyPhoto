6cb27e16fb290c41ec98ebf95828185c56c50e1f
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-01 20:07:27 -0600

    New pages

--- pages/header.php
+++ pages/header.php
@@ -1,24 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/headerfooter.css">
-
-    <title>Home</title>
-</head>
-<body>
-    <header>
-        <img src="../images/logo.PNG" alt="logo" width="100" height="100">
-        <h1>My Foto - Home</h1>
-    </header>
-    <nav>
-        <ul>
-            <li><a href="../myfoto/index.php">Home</a></li>
-            <li><a href="../pages/feed.php">Feed</a></li>
-            <li><a href="../pages/myprofile.php">My Profile</a></li>
-            <li><a href="../pages/about.php">About</a></li>
-        </ul>
-    </nav>
-</body>
-</html>
+<header>
+    <img src="../images/logo.PNG" alt="logo" width="100" height="100">
+    <h1>My Foto - Home</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="../myfoto/index.php">Home</a></li>
+        <li><a href="../pages/feed.php">Feed</a></li>
+        <li><a href="../pages/myprofile.php">My Profile</a></li>
+        <li><a href="../pages/about.php">About</a></li>
+    </ul>
+</nav>
