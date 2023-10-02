750b1cd1d5420c8f50a03be4ee508386dc8fd231
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-01 19:24:49 -0600

    New pages

---
+++ header.php
@@ -0,0 +1,25 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Your Website Title</title>
+</head>
+<body>
+    <header>
+        <img src="images/logo.PNG" alt="logo" width="100" height="100">
+        <h1>My Foto - Home</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="feed.php">Feed</a></li>
+            <li><a href="profile.php">My Profile</a></li>
+            <li><a href="about.php">About</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
+?>
---
+++ pages/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Welcome to My Foto</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+
+    <section id="welcome-section">
+        <h2>Create, Share, Inspire</h2>
+        <p>At My Foto, we believe that every picture tells a story. Capture your favorite moments and share them with the world.</p>
+        <a href="feed.php" class="cta-button">Explore the Feed</a>
+    </section>
+
+    <section id="featured-photos">
+        <h2>Featured Photos</h2>
+        <!-- Add a grid of featured photos here -->
+    </section>
+
+    <section id="testimonials">
+        <h2>What Our Users Say</h2>
+        <!-- Add user testimonials here -->
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <p>&copy; 2023 My Foto</p>
+    </footer>
+</body>
+</html>
