bdca7b6fd8de4af28e74d98ed33c9536c4ec74ae
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-18 15:02:40 -0600

    Adding database connection

--- server/myfotosql.sql
+++ server/myfotosql.sql
@@ -1,4 +1,39 @@
-CREATE DATABASE myfoto;
-USE myfoto;
+USE heroku_0bed022811aae1b;
+-- Create the 'users' table to store user information
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+-- Create the 'images' table to store user-uploaded images
+CREATE TABLE IF NOT EXISTS images (
+    image_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    image_path VARCHAR(255) NOT NULL,
+    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
-CREATE TABLE users
+-- Create the 'image_likes' table to store likes for images
+CREATE TABLE IF NOT EXISTS image_likes (
+    like_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    image_id INT NOT NULL,
+    like_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (image_id) REFERENCES images(image_id)
+);
+
+-- Create the 'comments' table to store comments on images
+CREATE TABLE IF NOT EXISTS comments (
+    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    image_id INT NOT NULL,
+    comment_text TEXT,
+    comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (image_id) REFERENCES images(image_id)
+);
+
+
