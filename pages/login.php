f014afbc51c0e52c55044cd343aa486e57dfc924
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-22 18:07:53 -0600

    Adding database connection

--- pages/login.php
+++ pages/login.php
@@ -32,7 +32,7 @@
         input[type="text"],
         input[type="password"] {
             width: 100%;
-            padding: 10px;
+            padding: 10px 10px, 10px, 10px;
             margin: 10px 0;
             border: 1px solid #ccc;
             border-radius: 5px;
