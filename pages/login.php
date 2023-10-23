245d83c0455bb863ec565312fb4d4f0654c1e7df
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-22 18:08:40 -0600

    Adding database connection

--- pages/login.php
+++ pages/login.php
@@ -32,7 +32,7 @@
         input[type="text"],
         input[type="password"] {
             width: 100%;
-            padding: 10px 10px, 10px, 10px;
+            padding: 5px 5px, 5px, 5px;
             margin: 10px 0;
             border: 1px solid #ccc;
             border-radius: 5px;
@@ -69,7 +69,7 @@
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
             <input type="submit" name="login" value="Login">
-            <a href="../pages/signup.php">Sign Up</a>
+            <a href="../pages/404.php">Sign Up</a>
         </form>
     </div>
 
