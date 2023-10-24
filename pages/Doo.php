47cb2b60b77fc53c87d9e0f0b892d55ce7913dc2
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-24 16:17:22 -0600

    Adding database connection

--- pages/Doo.php
+++ pages/Doo.php
@@ -15,10 +15,10 @@
         // }
     ?> -->
 <?php
-    private $host = "us-cdbr-east-06.cleardb.net";
-    private $db = "heroku_0bed022811aae1b";
-    private $user = "bd816edec88467";
-    private $pass = "104bcd5f";
+     $host = "us-cdbr-east-06.cleardb.net";
+     $db = "heroku_0bed022811aae1b";
+     $user = "bd816edec88467";
+     $pass = "104bcd5f";
 
     if(!$con = mysqli($host, $user, $pass, $db)){
 
