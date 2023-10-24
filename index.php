e285180e9c23e08de56c9a33b5e6a54af72bf7c0
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-24 17:06:17 -0600

    Adding database connection

--- index.php
+++ index.php
@@ -3,11 +3,7 @@ session_start();
 $_SESSION;
 
     require("pages/home.php"); 
-    require_once("pages/Doo.php");
-    // Create a new Database object
-    $db = new Database();
-    // Get a database connection
-    $connection = $db->getConnection();
+    
 
 ?>
 
