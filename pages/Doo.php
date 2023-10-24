5382c22be06b2d39030bbfbabefdc73ac8ce324e
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-24 17:01:04 -0600

    Adding database connection

--- pages/Doo.php
+++ pages/Doo.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection parameters
-private $host = "us-cdbr-east-06.cleardb.net";
-private $db = "heroku_0bed022811aae1b";
-private $user = "bd816edec88467";
-private $pass = "104bcd5f";
+$host = "us-cdbr-east-06.cleardb.net";
+$db = "heroku_0bed022811aae1b";
+$user = "bd816edec88467";
+$pass = "104bcd5f";
 
 // Create a database connection
 $mysqli = new mysqli($host, $user, $pass, $db);
