457359ec8b9ae026af81cc81b4db5667428c36ae
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-24 17:01:55 -0600

    Adding database connection

--- pages/Doo.php
+++ pages/Doo.php
@@ -8,8 +8,5 @@ $pass = "104bcd5f";
 // Create a database connection
 $mysqli = new mysqli($host, $user, $pass, $db);
 
-// Check the connection
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
+
 ?>
