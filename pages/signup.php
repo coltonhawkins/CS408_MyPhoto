78519b640fe455e4c680c3e2501c878103941fc6
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-24 17:29:10 -0600

    Adding database connection

--- pages/signup.php
+++ pages/signup.php
@@ -42,63 +42,32 @@ session_start();
 <body>
     <h1>My Foto - SignUp</h1>
 
-    <style>
-        #box {
-            width: 300px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #f0f0f0;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
+    <form>
+    
+        <div>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="Enter your Username">
+        </div>
 
-        form {
-            text-align: center;
-        }
+        <div>
+            <label for="email">email</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email">
+        </div>
 
-        input[type="text"],
-        input[type="password"] {
-            width: 100%;
-            padding: 5px 5px, 5px, 5px;
-            margin: 10px 0;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
+        <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password">
+        </div>
 
-        input[type="submit"] {
-            background-color: #007bff;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            padding: 10px 20px;
-            cursor: pointer;
-        }
+        <div>
+            <label for="password_confirmation">Confirm Password</label>
+            <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirm your password">
+        </div>
 
-        input[type="submit"]:hover {
-            background-color: #0056b3;
-        }
+        <button>Sign Up</button>
+    </form>
 
-        a {
-            display: block;
-            text-align: center;
-            margin-top: 10px;
-            text-decoration: none;
-            color: #007bff;
-        }
-
-        a:hover {
-            color: #0056b3;
-        }
-    </style>
-
-    <div id="box">
-        <form method="post">
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="submit" name="login" value="Signup">
-            <a href="../pages/login.php">Click to Login</a>
-        </form>
-    </div>
+   
 
     <?php include 'footer.php'; ?>
 </body>
