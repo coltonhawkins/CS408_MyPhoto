d95609f7168cf2c42c90ff19c91c8f43b39b267b
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-24 18:40:08 -0600

    Adding database connection

--- pages/process-signup.php
+++ pages/process-signup.php
@@ -33,7 +33,7 @@ if($_POST["password"] !== $_POST["password_confirmation"]) {
 //hashing the password
 $password_hash = password_hash($_POST["password_confirmation"], PASSWORD_DEFAULT);
 
-$mysqli = require __DIR__ . "/database.php";
+$mysqli = require __DIR__ . "/Doo.php";
 
 $sql = "INSERT INTO user (name, email, password_hash)
         VALUES (?, ?, ?)";
