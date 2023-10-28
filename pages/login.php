221804356547c90fed531accb2e90729b3cc3ebe
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-28 14:48:25 -0600

    Adding database connection

--- pages/login.php
+++ pages/login.php
@@ -40,19 +40,42 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
 </head>
 <?php include 'header.php'; ?>
 <body>
-    <style>
+<style>
+        /* Form container */
+        form {
+            width: 300px;
+            margin: 0 auto;
+            text-align: center;
+        }
+
+        /* Labels */
+        label {
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        /* Input fields */
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        /* Submit button */
         button {
-            background-color: #007BFF;
-            color: white;
+            background-color: #007bff;
+            color: #fff;
             padding: 10px 20px;
             border: none;
             border-radius: 5px;
-            font-size: 18px;
             cursor: pointer;
         }
 
+        /* Hover effect for the button */
         button:hover {
-            background-color: #0056b3; /* Darker color on hover */
+            background-color: #0056b3;
         }
     </style>
     <h1>My Foto - Login</h1>
