36dd22bfa6afa452a47276813d1a01c220e9a779
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-10-31 20:28:38 -0600

    Adding database connection

--- pages/myprofile.php
+++ pages/myprofile.php
@@ -21,6 +21,7 @@ if(isset($_SESSION["user_id"])) {
     
     <link rel="icon" href="../favicon/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="../favicon/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="../css/myprofile.css">
     <title>My Profile - My Foto</title>
 </head>
 <body>
