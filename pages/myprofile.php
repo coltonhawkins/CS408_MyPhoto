f72693405b90289588e41f20647d36e86a35664d
coltonhawkins <coltonhawkins@u.boisestate.edu>
2023-11-06 13:02:24 -0700

    Adding database connection

--- pages/myprofile.php
+++ pages/myprofile.php
@@ -52,7 +52,7 @@ if(isset($_SESSION["user_id"])) {
                             <h3>' . $row["titleGallery"] . '</h3>
                             <p>' . $row["descGallery"] . '</p>
                             <form action="delete_image.php" method="post">
-                                <input type="hidden" name="image_id" value="' . $row["idGallery"] . '">
+                                <input type="hidden" name="idGallery" value="' . $row["idGallery"] . '">
                                 <button type="submit" name="delete_image">Delete</button>
                             </form>
                         </div>
